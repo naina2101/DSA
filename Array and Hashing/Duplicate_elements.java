86d9a52381159ca49426a38fc4483a819035b3e3
Sunaina Singh <pt.naina1@gmail.com>
2024-12-14 12:18:08 +0530

    Complete

--- Array and Hashing/Duplicate_elements.java
+++ Array and Hashing/Duplicate_elements.java
@@ -1,7 +1,39 @@
+import java.util.*;
+
 class Duplicate_elements{
+
+    static boolean elements_check(int[] arr){
+        //Brute force - time complexity = O(n^2)
+        for(int i = 0; i < arr.length; i++)
+            for(int j = 0; j < arr.length; j++){
+                if(arr[i] == arr[j])
+                    return true;
+            }
+        
+        //sorting - time complexity = O(n logn)
+        Arrays.sort(arr);
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == arr[i-1])
+                return true;
+        }
+
+        //hashset - time complexity = O(n)
+        Set<Integer> seen = new HashSet<>();
+        for(int num : arr){
+            if(seen.contains(num))
+                return true;
+            seen.add(num);
+        }
+
+        //Hashset length - time complexity = O(n)
+        return Arrays.stream(arr).distinct().count() < arr.length;
+
+        return false;
+
+    }
     public static void main(String[] args) {
         int[] arr = {1, 2, 5, 7, 3, 2, 7};
-        
-
+        Boolean outcome = elements_check(arr);
+        System.out.println(outcome);
     }
 }
